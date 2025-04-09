b9e4df7a852ba8358d8b6102064c8f844638aafd
GuylhermeSS <163067197+GuylhermeSS@users.noreply.github.com>
2025-04-09 14:40:05 +0000

    Separada as execucoes por funcoes e adicionado os calculos de Densidade Polulacional e Pib per Capital

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-struct Carta {
+struct carta {
     char estado[2];
     char codigoCarta[3];
     char cidade[50];
@@ -8,67 +8,61 @@ struct Carta {
     float area;
     float pib;
     int numPontosTuristicos;
+    float densidadePopulacional;
+    float pibPerCapita;
 };
 
+void calculo(struct carta *val) {
+    val->densidadePopulacional = (float) val->populacao / val->area;
+    val->pibPerCapita = val->pib / val->populacao;
+}
+
+void entrada(struct carta *val) {
+    printf("Escolha um estado (Uma letra de A a H): ");
+    scanf("%s", val->estado);
+    printf("Escolha um codigo da carta (01 a 04): ");
+    scanf("%s", val->codigoCarta);
+    printf("Escolha uma cidade: ");
+    scanf("%s", val->cidade);
+    printf("Digite a quantidade de populacao: ");
+    scanf("%d", &val->populacao);
+    printf("Digite a area em KM (somente o numero): ");
+    scanf("%f", &val->area);
+    printf("Digite o pib: ");
+    scanf("%f", &val->pib);
+    printf("Escolha um numero de pontos turisticos: ");
+    scanf("%d", &val->numPontosTuristicos);
+    calculo(val);
+}
+
+void saida(struct carta *val){
+    printf("Estado: %s\n", val->estado);
+    printf("Codigo da Carta: %s%s\n", val->estado, val->codigoCarta);
+    printf("Cidade: %s\n", val->cidade);
+    printf("Populacao: %d\n", val->populacao);
+    printf("Area: %.2f KM²\n", val->area);
+    printf("PIB: %.2f bilhões de reais\n", val->pib);
+    printf("Pontos Turisticos: %d\n", val->numPontosTuristicos);
+    printf("Densidade Populacional: %.2f hab/km²\n", val->densidadePopulacional);
+    printf("PIB Per Capita: %.2f reais\n", val->pibPerCapita);
+}
+
 int main() {
-    struct Carta cartaUm = {" ", " ", " ", 0, 0.0, 0.0, 0};
-    struct Carta cartaDois = {" ", " ", " ", 0, 0.0, 0.0, 0};
+    struct carta cartaUm, cartaDois;
 
-    printf("Super Trunfo de Países\n\n");
-
-    printf("Iniciando Carta 1\n\n");
+    printf("Super Trunfo de Países");
     
-    printf("Escolha um estado (Uma letra de A a H): ");
-    scanf("%s", cartaUm.estado);
-    printf("Escolha um codigo da carta (01 a 04): ");
-    scanf("%s", cartaUm.codigoCarta);
-    printf("Escolha uma cidade: ");
-    scanf("%s", cartaUm.cidade);
-    printf("Digite a quantidade de populacao: ");
-    scanf("%d", &cartaUm.populacao);
-    printf("Digite a area em KM (somente o numero): ");
-    scanf("%f", &cartaUm.area);
-    printf("Digite o pib: ");
-    scanf("%f", &cartaUm.pib);
-    printf("Escolha um numero de pontos turisticos: ");
-    scanf("%d", &cartaUm.numPontosTuristicos);
+    printf("\n\nIniciando Carta Um\n\n");
+    entrada(&cartaUm);
 
-    printf("\n\nIniciando Carta 2\n\n");
-    
-    printf("Escolha um estado (Uma letra de A a H): ");
-    scanf("%s", cartaDois.estado);
-    printf("Escolha um codigo da carta (01 a 04): ");
-    scanf("%s", cartaDois.codigoCarta);
-    printf("Escolha uma cidade: ");
-    scanf("%s", cartaDois.cidade);
-    printf("Digite a quantidade de populacao: ");
-    scanf("%d", &cartaDois.populacao);
-    printf("Digite a area em KM (somente o numero): ");
-    scanf("%f", &cartaDois.area);
-    printf("Digite o pib: ");
-    scanf("%f", &cartaDois.pib);
-    printf("Escolha um numero de pontos turisticos: ");
-    scanf("%d", &cartaDois.numPontosTuristicos);
+    printf("\n\nIniciando Carta Dois\n\n");
+    entrada(&cartaDois);
 
     printf("\n\nEssas são as Cartas\n\n");
 
     printf("\n\nCarta 1\n");
-    printf("Estado: %s\n", cartaUm.estado);
-    printf("Codigo da Carta: %s%s\n", cartaUm.estado, cartaUm.codigoCarta);
-    printf("Cidade: %s\n", cartaUm.cidade);
-    printf("Populacao: %d\n", cartaUm.populacao);
-    printf("Area: %.2f KM\n", cartaUm.area);
-    printf("PIB: %.2f\n", cartaUm.pib);
-    printf("Pontos Turisticos: %d\n", cartaUm.numPontosTuristicos);
+    saida(&cartaUm);
 
     printf("\n\nCarta 2\n");
-    printf("Estado: %s\n", cartaDois.estado);
-    printf("Codigo da Carta: %s%s\n", cartaDois.estado, cartaDois.codigoCarta);
-    printf("Cidade: %s\n", cartaDois.cidade);
-    printf("Populacao: %d\n", cartaDois.populacao);
-    printf("Area: %.2f KM\n", cartaDois.area);
-    printf("PIB: %.2f\n", cartaDois.pib);
-    printf("Pontos Turisticos: %d\n", cartaDois.numPontosTuristicos);
-
-    return 0;
+    saida(&cartaDois);
 };
